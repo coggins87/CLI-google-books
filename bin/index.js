f026c11b10498d808f4826f4096cdd4593aa263a
Colleen <colleenhiggins87@gmail.com>
2019-11-05 12:41:05 -0800

    modularizing code

--- bin/index.js
+++ bin/index.js
@@ -1,33 +1,8 @@
 #!/usr/bin/env node
 
-const inquirer = require("./inquirer");
-const fs = require("fs");
 const yargs = require("yargs");
-const search = require("./book-search");
-
-const start = async () => {
-  let searchTerm = await inquirer.getSearch();
-  if (searchTerm.search) {
-    let answers = await search.bookSearch(searchTerm.search);
-    let bookToSave = await inquirer.createList(answers);
-    if (bookToSave) {
-      if (fs.existsSync("./saved-books.txt")) {
-        fs.appendFile("./saved-books.txt", bookToSave.results + "\n", err => {
-          if (err) throw err;
-          console.log(`The ${bookToSave.results} was added to your book list!`);
-        });
-      } else {
-        fs.writeFile("./saved-books.txt", bookToSave.results, err => {
-          if (err) throw err;
-          console.log(
-            `The ${bookToSave.results} was added to your new book list file!`
-          );
-        });
-       
-      }
-    }
-  } else console.log("Enter a search term to see a list of books");
-};
+const start = require("./start-cli")
+const fs = require("fs");
 
 options = yargs
   .usage("$0 <cmd> [args]")
---
+++ bin/start-cli.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const search = require("./book-search");
+const inquirer = require("./inquirer");
+
+
+const start = async () => {
+  let searchTerm = await inquirer.getSearch();
+  if (searchTerm.search) {
+    let answers = await search.bookSearch(searchTerm.search);
+    let bookToSave = await inquirer.createList(answers);
+    if (bookToSave) {
+      if (fs.existsSync("./saved-books.txt")) {
+        fs.appendFile("./saved-books.txt", bookToSave.results + "\n", err => {
+          if (err) throw err;
+          console.log(`The ${bookToSave.results} was added to your book list!`);
+        });
+      } else {
+        fs.writeFile("./saved-books.txt", bookToSave.results, err => {
+          if (err) throw err;
+          console.log(
+            `The ${bookToSave.results} was added to your new book list file!`
+          );
+        });
+       
+      }
+    }
+  } else console.log("Enter a search term to see a list of books");
+};
+
+module.exports = start
